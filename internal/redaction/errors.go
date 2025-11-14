bc4f12c57e937a5659f85a46b64a610bf134b59e
Issei Suzuki <4515431+isseis@users.noreply.github.com>
2025-11-14 06:52:50 +0000

    refactor: removed unused ErrRegexCompilationFailed type

--- internal/redaction/errors.go
+++ internal/redaction/errors.go
@@ -13,13 +13,3 @@ type ErrLogValuePanic struct {
 func (e *ErrLogValuePanic) Error() string {
 	return fmt.Sprintf("LogValue() panicked for attribute %q: %v", e.Key, e.PanicValue)
 }
-
-// ErrRegexCompilationFailed is returned when regex compilation fails
-type ErrRegexCompilationFailed struct {
-	Pattern string
-	Err     error
-}
-
-func (e *ErrRegexCompilationFailed) Error() string {
-	return fmt.Sprintf("failed to compile regex pattern %q: %v", e.Pattern, e.Err)
-}
